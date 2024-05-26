7ac9dcc4d5846fa492ae732859ef82432e7615e9
windows <egor.shmanev@gmail.com>
2024-05-27 02:20:35 +0300

    J4

--- algorithms/Johnsons2/myblog/blog/urls.py
+++ algorithms/Johnsons2/myblog/blog/urls.py
@@ -1,10 +1,20 @@
-# blog/urls.py
-from django.urls import path
-# from .views import post_list, engineer_list
-from .views import  engineer_list
+# # blog/urls.py
+# from django.urls import path
+# # from .views import post_list, engineer_list
+# from .views import  engineer_list
 
+
+# urlpatterns = [
+#     # path('', post_list, name='post_list'),
+#     path('engineers/', engineer_list, name='engineer_list'),
+# ]
+
+# blog/urls.py
+
+from django.urls import path
+from .views import engineer_list, schedule_tasks
 
 urlpatterns = [
-    # path('', post_list, name='post_list'),
     path('engineers/', engineer_list, name='engineer_list'),
+    path('schedule/', schedule_tasks, name='schedule_tasks'),
 ]
--- algorithms/Johnsons2/myblog/blog/utils.py
+++ algorithms/Johnsons2/myblog/blog/utils.py
@@ -1,3 +1,5 @@
+# blog/utils.py
+
 def johnson_scheduling(tasks):
     """
     Распределение задач между двумя инженерами с использованием алгоритма Джонсона.
@@ -21,13 +23,3 @@ def johnson_scheduling(tasks):
             schedule[task_index] = "Инженер 2"
 
     return schedule
-
-# Пример задач
-tasks_list = [(3, 2), (1, 4), (5, 1), (2, 3), (4, 2)]
-
-# Получаем оптимальное расписание
-optimal_schedule = johnson_scheduling(tasks_list)
-
-# Выводим результат
-for i, task in enumerate(optimal_schedule):
-    print(f"Задача {chr(65 + i)}: {task}")
